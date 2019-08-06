c74ff70b7729913cf12b9211ff67b6fc92147660
Randy J. Ray <rjray@blackperl.com>
2019-08-05 23:00:39 -0700

    Removed diagnostics

--- server.js
+++ server.js
@@ -11,9 +11,4 @@ app.use("/api", api)
 app.get("/", (req, res) => res.send("OK"))
 
 console.log("Server started")
-console.log(`-> NODE_ENV=${process.env.NODE_ENV}`)
-for (let name of ["USERNAME", "PASSWORD", "DATABASE", "HOST"]) {
-  let full = `DB_${name}`
-  console.log(`-> ${full}=${process.env[full]}`)
-}
 app.listen(port)
