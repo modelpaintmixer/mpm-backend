a6a353a5d29356f40aab166693cba47118784f03
Randy J. Ray <rjray@blackperl.com>
2019-08-26 16:00:47 -0700

    Extend fetching to include user data

--- routes/api/newsitem.js
+++ routes/api/newsitem.js
@@ -4,21 +4,23 @@
 
 const express = require("express")
 
-const { NewsItem } = require("../../models")
+const { User, NewsItem } = require("../../models")
 
 let router = express.Router()
 
 router.get("/", (req, res) => {
-  NewsItem.findAll({ order: [["updatedAt", "DESC"]] }).then(results => {
-    let newsitems = results.map(item => item.get())
-    res.send({ newsitems, timestamp: Date.now() })
-  })
+  NewsItem.findAll({ order: [["updatedAt", "DESC"]], include: [User] }).then(
+    results => {
+      let newsitems = results.map(item => item.get())
+      res.send({ newsitems, timestamp: Date.now() })
+    }
+  )
 })
 
 router.get("/:id", (req, res) => {
   let id = req.params.id
 
-  NewsItem.findByPk(id).then(newsitem => {
+  NewsItem.findByPk(id, { include: [User] }).then(newsitem => {
     newsitem = newsitem.get()
 
     res.send({ newsitem, timestamp: Date.now() })
