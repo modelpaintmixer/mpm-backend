fd90d517146dd40ee1d2b78f0912b8c5c8f0eb75
Randy J. Ray <rjray@blackperl.com>
2019-09-04 16:14:56 -0700

    Made this work (no-op) with SQLite

--- migrations/00000-fix-db-charset.js
+++ migrations/00000-fix-db-charset.js
@@ -2,10 +2,14 @@
 
 module.exports = {
   up: queryInterface => {
-    return queryInterface.sequelize.query(
-      `ALTER DATABASE ${queryInterface.sequelize.config.database}
-        CHARACTER SET utf8 COLLATE utf8_general_ci;`
-    )
+    if (queryInterface.sequelize.getDialect() === "mysql") {
+      return queryInterface.sequelize.query(
+        `ALTER DATABASE ${queryInterface.sequelize.config.database}
+          CHARACTER SET utf8 COLLATE utf8_general_ci;`
+      )
+    } else {
+      return Promise.resolve(null)
+    }
   },
   down: () => {},
 }
